962e3d4cb138f1ab80e3dc278c29e25f8ea48e45
AnastasijaAndrejeva3 <125015510+AnastasijaAndrejeva3@users.noreply.github.com>
2023-03-03 23:35:58 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,55 +1,50 @@
 import sys
-import time
 import threading
-from collection import nametuple
-import namedtuple
 
-elem = namedtuple("elem", ["vertiba", "dzilums", "apmeklējums"])
+def compute_height(n, parents):
+    m = [[] for _ in range(n)]
+    root = None
 
-def main(): 
-    ievade = input ()
-    text = ""
-    count = 0
-    if "I" in ievade: 
-        count = int(input())
-        text = input() 
-    elif "F" in ievade:
-        FName = input("Ievadiet faila nosaukumu: ")
-        if "a" in FName:
-            print("Faila nosaukumā nedrīkst būt burts 'a'")
+    for i, parent in enumerate(parents):
+        if parent == -1:
+            root = i    
         else:
-            pl = time.time()
-            with open(FName, mode="r") as file: 
-                count = int(file.readline ())
-                text = file.readline()
+            m[parent].append(i)
 
-    text = text.split()
-    text = list(map(int, text)) 
-    m = [elem(val, 0, False) for val in text]
-    max_height = 0
-    for i in range (count) :
-        val = m[i]
-        if not val.apmeklējums:
-            tail [i]
-            height = 1
-            while val.vertiba != -1:
-                tail.append(val.vertiba)
-                val = m[val.vertiba]
-                height += 1
-                if val.apmeklējums:
-                    height += val.dzilums
-                    break
-                if height > max_height:
-                    max_height = height
-            n = val.dzilums
-            for j in range(len(tail) -1, -1, -1):
-                node = tail[j]
-                if m[node] = elem(m[node].vertiba, n, True)
-                n += 1
+    def max_height(value):
+        tree = 1
+        
+        if not m[value]:
+            return tree
+        else:
+            for child in m[value]:
+                tree = max(tree, max_height(child))
 
-    print (max_height) 
-#laiks2 = time.time()
-#print (laiks2 - laiks1)
-sys.setrecursionlimit (10**7)
-threading.stack_size (2**27) 
-threading. Thread (target-main).start()
+            return tree + 1
+    return max_height(root)
+
+def main():
+    text = input()
+    if "I" in text:
+        n = int(input())
+        parents = list(map(int, input().split()))
+    elif "F" in text:
+ 
+        file = input()
+        if "a" in file:
+                print("Error")
+                return      
+        else:
+            with open (file, mode="r") as file: 
+                count = int (file.readline ())
+                text = file.readline ()
+        
+    else:
+        print("Ievadiet burtu 'I' vai 'F':")
+        return
+            
+    print(compute_height(n, parents))
+
+sys.setrecursionlimit(10**7)  
+threading.stack_size(2**27)   
+threading.Thread(target=main).start()
