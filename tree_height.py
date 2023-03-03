efc1c2a990ca58ba63fd47401229ea009ddb92e9
AnastasijaAndrejeva3 <125015510+AnastasijaAndrejeva3@users.noreply.github.com>
2023-03-03 21:55:47 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,33 +1,55 @@
-# python3
-
 import sys
+import time
 import threading
-import numpy
+from collection import nametuple
+import namedtuple
 
+elem = namedtuple("elem", ["vertiba", "dzilums", "apmeklējums"])
 
-def compute_height(n, parents):
-    # Write this function
+def main(): 
+    ievade = input ()
+    text = ""
+    count = 0
+    if "I" in ievade: 
+        count = int(input())
+        text = input() 
+    elif "F" in ievade:
+        FName = input("Ievadiet faila nosaukumu: ")
+        if "a" in FName:
+            print("Faila nosaukumā nedrīkst būt burts 'a'")
+        else:
+            pl = time.time()
+            with open(FName, mode="r") as file: 
+                count = int(file.readline ())
+                text = file.readline()
+
+    text = text.split()
+    text = list(map(int, text)) 
+    m = [elem(val, 0, False) for val in text]
     max_height = 0
-    # Your code here
-    return max_height
+    for i in range (count) :
+        val = m[i]
+        if not val.apmeklējums:
+            tail [i]
+            height = 1
+            while val.vertiba != -1:
+                tail.append(val.vertiba)
+                val = m[val.vertiba]
+                height += 1
+                if val.apmeklējums:
+                    height += val.dzilums
+                    break
+                if height > max_height:
+                    max_height = height
+            n = val.dzilums
+            for j in range(len(tail) -1, -1, -1):
+                node = tail[j]
+                if m[node] = elem(m[node].vertiba, n, True)
+                n += 1
 
-
-def main():
-    # implement input form keyboard and from files
-    
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-    pass
-
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
-threading.Thread(target=main).start()
-main()
-# print(numpy.array([1,2,3]))
+    print (max_height) 
+#laiks2 = time.time()
+#print (laiks2 - laiks1)
+sys.setrecursionlimit (10**7)
+threading.stack_size (2**27) 
+threading. Thread (target-main).start()
