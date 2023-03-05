b4d2ce4d0451e6283be8bb56406dde1ff691aa72
AnastasijaAndrejeva3 <125015510+AnastasijaAndrejeva3@users.noreply.github.com>
2023-03-05 12:43:43 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -31,7 +31,7 @@ def main():
         parents = list(map(int, input().split()))
     elif "F" in text:
         fileName = input()
-	    path = './test/'    
+        path = './test/'    
         folder = os.path.join(path, fileName)
         if "a" not in fileName:
             try:
@@ -51,6 +51,7 @@ def main():
         return
             
     print(compute_height(n, parents))
+
 sys.setrecursionlimit(10**7)  
 threading.stack_size(2**27)   
 threading.Thread(target=main).start()
